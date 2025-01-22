81d458c988950d97fa4e808a3c7057532ef257b7
goodbyebada <goodbyebada@naver.com>
2025-01-23 03:29:53 +0900

    Feat :  채팅 UI component 추가
    - MessageInput
    - MessageItem
    - Chanel : 채팅 메시지(MessageItem), 채팅 입력 폼(MessageInput)
    - ChatRoomPage : 채팅방

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ManagerDashboard from "./pages/ManagerDashboard";
+
 import AddNotice from "./pages/AddNotice";
 import ShowApplicant from "./pages/ShowApplicant";
 
@@ -44,7 +44,10 @@ import PostOverviewPage from "@pages/PostOverviewPage";
 import RecruitmentStatus from "@pages/RecruitmentStatus";
 import { useEffect } from "react";
 import handleAllowNotification from "@utills/pushNotification/notificationPermission";
-import Chat from "@pages/Chat/Chat";
+import ChatList from "@pages/Chat/ChatList";
+import Channel from "@pages/Chat/Chanel";
+import ManageOverViewPage from "@pages/ManageOverViewPage";
+import ChatRoomPage from "@pages/Chat/ChatRoomPage";
 
 function App() {
   useEffect(() => {
@@ -120,7 +123,7 @@ function App() {
           <Route path="/company/manage/clock-in" element={<ClockInPage />} />
           <Route path="/company/manage/clock-out" element={<ClockOutPage />} />
           {/* company notice */}
-          <Route path="/company/notice" element={<ManagerDashboard />} />
+          <Route path="/company/notice" element={<ManageOverViewPage />} />
           {/* 업체 측 공고 리스트 페이지 화면 */}
           <Route
             path="/company/notice/post-overview"
@@ -145,11 +148,6 @@ function App() {
             element={<CompanySettingPage />}
           />
 
-          <Route
-            path="/company/manager-dashboard"
-            element={<ManagerDashboard />}
-          />
-
           <Route path="/applicants" element={<ShowApplicant />} />
           <Route path="/applicants/detail" element={<ApplicantDetail />} />
           <Route path="/detail/:id" element={<DetailPage />} />
@@ -165,7 +163,9 @@ function App() {
           {/* for testing theme */}
           <Route path="/theme-preview" element={<ThemePreviewPage />} />
 
-          <Route path="/chatRoom" element={<Chat />} />
+          <Route path="/chatRoom" element={<ChatList />} />
+          <Route path="/chatRoom/channel/tmp" element={<ChatRoomPage />} />
+          <Route path="/chatRoom/channel/:id" element={<ChatRoomPage />} />
         </Routes>
       </BrowserRouter>
     </>
